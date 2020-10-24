3b8f464e93d2a3c0081a6ae08d3a22e261a5b17c
Admin <>
2020-10-24 15:10:45 +0200

    hardcode github api request

--- app/main.py
+++ app/main.py
@@ -6,11 +6,15 @@ TODO:
 - get the state from the repository
 
 """
+import requests
 import time
 
-
 def get_current_commit_from_github_repo():
-    raise NotImplementedError
+    response = requests.get("https://api.github.com/repos/eduardknezovic/git2sms/commits")
+    data = response.json()
+    last = data[0]
+    message = last['commit']['message']
+    return message
 
 def get_last_remembered_commit():
     return None
@@ -22,9 +26,11 @@ def main():
     remembered_commit = get_last_remembered_commit()
     while True:
         current_commit = get_current_commit_from_github_repo()
-        if remembered_commit is not current_commit:
+        if remembered_commit != current_commit:
             send_sms()
             remembered_commit = current_commit # Update state
+        else:
+            print("Sadness")
         time.sleep(5)
 
 if __name__ == "__main__":
