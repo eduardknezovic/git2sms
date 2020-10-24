49d3bda5bf56756d9031bc9223929c74d9629c38
Admin <>
2020-10-24 14:49:38 +0200

    add skeleton

--- app/main.py
+++ app/main.py
@@ -1,1 +1,31 @@
 
+
+"""
+TODO:
+
+- get the state from the repository
+
+"""
+import time
+
+
+def get_current_commit_from_github_repo():
+    raise NotImplementedError
+
+def get_last_remembered_commit():
+    return None
+
+def send_sms():
+    print("The repo has been updated!")
+
+def main():
+    remembered_commit = get_last_remembered_commit()
+    while True:
+        current_commit = get_current_commit_from_github_repo()
+        if remembered_commit is not current_commit:
+            send_sms()
+            remembered_commit = current_commit # Update state
+        time.sleep(5)
+
+if __name__ == "__main__":
+    main()
